c69832a6909c685b5ea8d6646c91ac37b4883242
Saad Bin Khalid <102467366+saad0510@users.noreply.github.com>
2025-11-01 21:22:57 +0500

    Updated README.md

--- lib/app/constants.dart
+++ lib/app/constants.dart
@@ -3,9 +3,8 @@ import 'package:flutter/rendering.dart';
 class AppConstants {
   static const designSize = Size(375, 812);
   static const title = "Emirates";
-  static const oAuthKey = "887969691409-dj3aa9tpa9guk5mbt65tkmg6ubcqoum0.apps.googleusercontent.com";
+  static const oAuthKey = "apps.googleusercontent.com";
   static const playstoreLink = "git@github.com:saad0510/emirates-app.git";
-  static const supabaseUrl = "https://owquxhlxmutcrcifvfsy.supabase.co";
-  static const supabaseAnonKey =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93cXV4aGx4bXV0Y3JjaWZ2ZnN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjkxMDMwOTUsImV4cCI6MTk4NDY3OTA5NX0._hxo7zlCRr1FPYSu19l61sAva6KTEQuWv7VwDEbAsk8";
+  static const supabaseUrl = "https://supabase.co";
+  static const supabaseAnonKey = "";
 }
